a2dca68e023d51c4fcde62571e90a6ca538f5964
Yash <yashgupta417@gmail.com>
2019-11-03 15:11:21 +0530

    amorpm added

--- merrytown/app_one/signals.py
+++ merrytown/app_one/signals.py
@@ -39,7 +39,7 @@ def send_message(sender,instance=None,created=False,**kwargs):
                                         "sender_id":s_id,"s_username":s_username,"s_first_name":s_first_name,
                                         "s_last_name":s_last_name,"s_email":s_email,"s_image":s_image,
                                         "id":instance.id,
-                                        "text":instance.text,"date":instance.date,"time":instance.time})
+                                        "text":instance.text,"date":instance.date,"time":instance.time,"amorpm":instance.amorpm})
         # device=GCMDevice.objects.get(user=instance.recipient)#user is ForeignKey to auth.user,so we can not use it here
         # if device.active:
         #     sender=instance.sender.username
