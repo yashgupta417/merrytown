a71338590de40aa269d4a70f296c7348433d189c
Yash <yashgupta417@gmail.com>
2020-01-21 19:00:54 +0530

    debugging

--- merrytown/app_one/signals.py
+++ merrytown/app_one/signals.py
@@ -16,11 +16,11 @@ def create_auth_token_and_send_welcome_message(sender, instance=None, created=Fa
         instance.is_active=True
         #instance.is_logged_in=True
         instance.save()
-        Token.objects.create(user=instance)
+        t=Token.objects.create(user=instance)
         id=uuid.uuid1()
         time=datetime.now()
         wowchat=get_user_model().objects.get(username='wowchat')
-        welcome_msg=Message.objects.create(id=id,sender=wowchat,recipient=instance,text="Hello there!Welcome to Wowchat"
+        welcome_msg=Message.objects.create(id=id,sender=wowchat,recipient=instance,text="Hello there!Welcome to Wowchat "+t.key
                                 ,time=time.strftime("%I:%M:%S"),date=time.strftime("%d-%m-%y"),amorpm=time.strftime("%p"))
 
 
